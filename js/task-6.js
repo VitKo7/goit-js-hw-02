c2fb9604e4271d2e62babd787ec8d956603ae176
Vitaliy Kornienko <Kornienko.Vitaliy@gmail.com>
2020-12-21 17:03:31 +0200

    mistakes corrected

--- js/task-6.js
+++ js/task-6.js
@@ -5,25 +5,43 @@ const numbers = [];
 let total = 0;
 let message;
 
-while (true) {
+while (userInput !== null) {
   userInput = prompt('Введите число:');
 
   if (!+userInput) {
     if (userInput) {
       alert('Было введено не число, попробуйте еще раз!');
     }
-    for (let i = 0; i < numbers.length; i += 1) {
-      total += +numbers[i];
-    }
-    message = `Общая сумма чисел равна ${total}`;
-    break;
+  } else {
+    numbers.push(userInput);
   }
-
-  numbers.push(userInput);
 }
+for (let i = 0; i < numbers.length; i += 1) {
+  total += +numbers[i];
+}
+
+message = `Общая сумма чисел равна ${total}`;
 console.log(message);
 
 // continue;
 // alert('Отменено пользователем');
 // userInput = Number(userInput);
 // if (Number.isNaN(userInput)) {
+
+// while (true) {
+//   userInput = prompt('Введите число:');
+
+//   if (!+userInput) {
+//     if (userInput) {
+//       alert('Было введено не число, попробуйте еще раз!');
+//     }
+//     for (let i = 0; i < numbers.length; i += 1) {
+//       total += +numbers[i];
+//     }
+//     message = `Общая сумма чисел равна ${total}`;
+//     break;
+//   }
+
+//   numbers.push(userInput);
+// }
+// console.log(message);
